f5d0ed5961d97f9994586bc021e517a0633bfc00
Jose Castillo <josedavidcastilloospina@gmail.com>
2024-02-29 17:51:15 -0500

    Se da estilos y se agregan imagenes

--- findy/src/componentes/Perfil.tsx
+++ findy/src/componentes/Perfil.tsx
@@ -10,7 +10,7 @@ export default function Perfil() {
       <div>
         <div className="profile-container">
           <div className="profile-header">
-            <img src="https://res.cloudinary.com/dwpzqxijx/image/upload/v1709070674/AppPerfil/jennie-blackpink-uhdpaper.com-hd-4_1_s6vjyi.png" alt="Profile Picture" className="profile-image" />
+            <img src="https://res.cloudinary.com/dwpzqxijx/image/upload/v1709070674/AppPerfil/jennie-blackpink-uhdpaper.com-hd-4_1_s6vjyi.png" alt="Profile Picture" className="profile-image"/>
             <div className="profile-name">Jennie Kim</div>
           </div>
           <div className="profile-stats">
@@ -33,10 +33,19 @@ export default function Perfil() {
           </div>
         </div>
         <div className="menu-profile">
-          <button className="photos">Photos</button>
-          <button className="videos">Videos</button>
-          <button className="album">Album</button>
-          <button className="tag">Tag</button>
+          <div className="menu-buttons">
+            <button className="photos">Photos</button>
+            <button className="videos">Videos</button>
+            <button className="album">Album</button>
+            <button className="tag">Tag</button>
+          </div>
+          <br />
+          <div className="images-profile">
+            <img src="https://res.cloudinary.com/dwpzqxijx/image/upload/AppPerfil/Group_15_qnrnwh.jpg" alt=""/>
+            <img src="https://res.cloudinary.com/dwpzqxijx/image/upload/v1709070863/AppPerfil/Group_16_of6dsb.png" alt="" />
+            <img src="https://res.cloudinary.com/dwpzqxijx/image/upload/v1709070905/AppPerfil/Group_17_e0e1mh.png" alt="" />
+            <img src="https://res.cloudinary.com/dwpzqxijx/image/upload/v1709070907/AppPerfil/Group_19_aziywo.png" alt="" />
+          </div>
         </div>
       </div>
       <Footer />
